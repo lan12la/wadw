4fc92f3bca8a9b5d431904e315e2b3b8eba264c8
lan12la <135573246+lan12la@users.noreply.github.com>
2023-06-19 12:00:51 +0300

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,14 @@
+const config = { 
+//------------Connections-----------//
+    Client_Token: 'MTEwNTg4NTEwODA4NDQyMDY4OQ.GrLSs2.vDKmHX-DdCcwPzLmW4OKQo-nKFNYk-LWqhZgf0',
+    MongoDB_ConnectURL: 'mongodb+srv://LWZcode:1234onan@cluster0.kjske.mongodb.net/LWZcode?retryWrites=true&w=majority',
+    //------------BotSettings-----------//
+    Prefix: '.',
+    BotOwners: ["1088738527702954094"],
+    Custom_Status_Text: 'Atlantic Park ❤️ Charonn',
+    Custom_Status_Type: 'PLAYING', // => PLAYING / WATCHING / LISTENING
+    Custom_Status: 'idle', // => dnd / idle / online / invisible
+    VoiceChannelID: '1118482458204180550'
+};
+  
+module.exports = config;
